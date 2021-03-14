9f1c378e3e5e273dbd4a0dd6992e3a11b269f75e
ThomasLuca <thomasluca008@gmail.com>
2021-03-14 18:05:06 +0100

    ⚙  Made all settings accessible via frontend

--- frontend/src/views/Settings.vue
+++ frontend/src/views/Settings.vue
@@ -1,9 +1,17 @@
 <template>
     <div>
       <h1>Settings</h1>
-      <div style="text-align: left; margin: 0 18%;">
+      <div style="text-align: left; margin: 0 12%;">
         <h3>Wanted Temperature</h3>
+        <div class="slidecontainer">
+          <input type="range" min="0" max="60" class="slider" step="5" v-model="wantedTemperature">
+          <p>{{ wantedTemperature }}°C</p>
+        </div>
         <h3>Wanted Temperature Range</h3>
+        <div class="slidecontainer">
+          <input type="range" min="0" max="5" class="slider" step="0.1" v-model="wantedTemperatureRange">
+          <p>{{ wantedTemperatureRange }}°C</p>
+        </div>
         <h3>Brightness</h3>
         <div class="slidecontainer">
           <input type="range" min="0" max="255" class="slider" step="5" v-model="brightness">
@@ -14,8 +22,15 @@
           <input type="range" min="0" max="0.150" class="slider" step="0.005" v-model="displaySpeed">
           <p>{{ displaySpeed }}</p>
         </div>
-        <h3>Rotation</h3>
-        <button @click="submitSettings">Submit Settings</button>
+        <h3>Text direction</h3>
+        <div class="rotations">
+          <button @click="changeDirection(0)">→</button>
+          <button @click="changeDirection(90)">↓</button>
+          <button @click="changeDirection(180)">←</button>
+          <button @click="changeDirection(270)">↑</button>
+          <p>{{ rotation }}°</p>
+        </div>
+        <button @click="submitSettings" id="submit">Submit Settings</button>
       </div>
     </div>
 </template>
@@ -25,8 +40,11 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   data() {
     return {
-      brightness: 255,
-      displaySpeed: 0.075,
+      wantedTemperature: "36.0",
+      wantedTemperatureRange: "1.0",
+      brightness: "255",
+      displaySpeed: "0.075",
+      rotation: 180,
     }
   },
   mounted() {
@@ -39,8 +57,20 @@ export default defineComponent({
   },
   methods: {
     submitSettings(){
-      console.log("submitting") 
-      this.$store.dispatch('postSettings', {"settings": {"brightness": this.brightness, "display_speed": this.displaySpeed}})
+      this.$store.dispatch('postSettings', 
+      {
+        "settings": 
+        {
+          "wanted_temperature": parseInt(this.wantedTemperature),
+          "wanted_temperature_range": parseFloat(this.wantedTemperatureRange),
+          "brightness":  parseInt(this.brightness), 
+          "display_speed": parseFloat(this.displaySpeed), 
+          "rotation": this.rotation
+        }
+      })
+    },
+    changeDirection(angle: number){
+      this.rotation = angle;
     }
   },
 })
@@ -48,6 +78,24 @@ export default defineComponent({
 
 <style>
 .slider {
-  height: 0px;
+  height: 85px;
 }
+
+.rotations{
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 40px;
+}
+.rotations > button {
+  font-size: 45px;
+}
+.slidecontainer {
+  display: flex;
+}
+#submit {
+  margin-bottom: 50px;
+  font-size: 30px;
+  
+}
+
 </style>
