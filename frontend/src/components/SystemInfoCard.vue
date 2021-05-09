40e3b1f60acfb979a9215cc55ea79145055156b9
ThomasLuca <thomasluca008@gmail.com>
2021-05-09 17:35:26 +0200

    added components for systeminfo

---
+++ frontend/src/components/SystemInfoCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="card blue-grey lighten-5">
+        <div class="card-content black-text">
+            <span class="card-title">{{title}}</span>
+            <p>{{value}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+export default {
+    props: {title: String, value: String}
+}
+</script>
+
+<style>
+.card{
+    width: 330px;
+    margin-bottom: 30px;
+}
+</style>
--- frontend/src/views/SystemInfo.vue
+++ frontend/src/views/SystemInfo.vue
@@ -1,20 +1,28 @@
 <template>
-    <div>
-        <h1>System Info</h1>
-        <li v-for="(element, k) in systemInfo" :key="k">
-          <b>{{k}}:</b> {{ element }}
-        </li>
-    </div>
+  <div>
+    <h1>System Info</h1>
+    <div class="card-container">
+      <SystemInfoCard
+        v-for="(element, k) in systemInfo" :key="k"
+        :title="k"
+        :value="element">
+      </SystemInfoCard>
+    </div>  
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import SystemInfoCard from '../components/SystemInfoCard.vue'
+
 export default defineComponent({
+  components: { SystemInfoCard },
   mounted() {
     this.$store.dispatch('getSystemInfo')
   },
   computed: {
     systemInfo() {
+      console.log(this.$store.state.system)
       return this.$store.state.system
     }
   }
@@ -22,7 +30,9 @@ export default defineComponent({
 </script>
 
 <style>
-li {
-  font-size: 28px;
+.card-container{
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
 }
 </style>
